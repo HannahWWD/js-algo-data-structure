8dbbc537a981affd88e24fb2ef7fc6d35ea36e72
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2020-09-03 13:35:13 -0700

    steps/pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,78 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+/* solution 3 */
 
+function pyramid(n,row=0,stairs=""){
+    if(n === row){
+        return;
+    }
+    if(stairs.length === 2 * n - 1){
+        // stair.length is the same as the index of the column
+        console.log(stairs)
+        // move to another row when reaching to the end of the row (reaching to the last column)
+        // this step will change the row value to 1
+        // this step also means reset stairs(Str) to ''
+        pyramid(n, row + 1);
+        return;
+    }
+
+    const midpoint = Math.floor(( 2*n-1)/2);
+
+    if(midpoint - row <= stairs.length && midpoint + row >= stairs.length){
+        stairs += '#';
+    }else {
+        stairs += ' ';
+    }
+    // move to next column in the same row
+    // this will maintain the value of the row and stairs (0," ")
+    pyramid(n,row,stairs)
+
+}
+
+
+
+pyramid(4)
 module.exports = pyramid;
+
+/* solution 1 */
+
+// function pyramid(n) {
+//     // think about how n is related to columns
+//     for (let row = 0; row < n; row++) {
+//         let eachStep = '';
+//         for (let col = 0; col < 2 * n - 1; col++) {
+//             if (col < n - row - 1) {
+//                 eachStep += " ";
+//             } else if (col < n + row) {
+//                 eachStep += "#";
+//             } else {
+//                 eachStep += " "
+//             }
+
+//         }
+//         console.log(eachStep)
+
+//     }
+
+
+
+// }
+
+/* solution 2 */
+
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     for (let row = 0; row < n; row++) {
+//         let eachStep = '';
+//         for (let col = 0; col < 2 * n - 1; col++) {
+//             if (midpoint - row <= col && midpoint + row >= col) {
+//                 eachStep += '#'
+
+//             } else {
+//                 eachStep += ' '
+//             }
+//         }
+//         console.log(eachStep);
+//     }
+// }
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,60 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
 
+function steps(n, row=0, stairs=''){
+    if(n === row){
+        return;
+    }
+    if(n === stairs.length){
+        // stair.length is the same as the index of the column
+        console.log(stairs)
+        // move to another row when reaching to the end of the row (reaching to the last column)
+        // this step will change the row value to 1
+        // this step also means reset stairs(Str) to ''
+        steps(n, row + 1);
+        return;
+    }
+
+    if(stairs.length <= row){
+        stairs += '#';
+    }else {
+        stairs += ' ';
+    }
+    // move to next column in the same row
+    // this will maintain the value of the row and stairs (0,"# ")
+    steps(n,row,stairs)
+
+}
+
+
+steps(2)
 module.exports = steps;
+
+
+/* solution 1 */
+// function steps(n) {
+//     const symbol = "#";
+//     for (let i=0; i<n;i++){
+//         const eachStep = "#".repeat(i+1) + " ".repeat(n-1-i);
+//         console.log(eachStep)
+
+//     }
+// }
+
+
+
+/* solution 2 */
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let eachStep = "";
+//         for (let col = 0; col < n; col++) {
+//             if (col <= row) {
+//                 eachStep = eachStep + "#";
+//             } else {
+//                 eachStep = eachStep + " ";
+//             }
+//         }
+//         console.log(eachStep)
+//     }
+// }
